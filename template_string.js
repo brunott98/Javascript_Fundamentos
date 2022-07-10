268ab5a65516f1fe3cd1aa5246c16ace67ad78cd
Bruno <104589217+brunott98@users.noreply.github.com>
2022-07-09 23:57:01 -0300

    Aplicações de Template Strings

---
+++ template_string.js
@@ -0,0 +1,15 @@
+const segundo_nome = "ARATAKI";
+const primeiro_nome = " ITTO";
+const nome_completo = (segundo_nome + primeiro_nome);
+
+const diminui = texto => texto.toLowerCase();
+
+const boas_vindas =`
+    Bem vindo(a)
+    ${diminui(nome_completo)}!`
+
+console.log(boas_vindas)    
+
+
+console.log(`\t1+1 = ${1 + 1}`)
+
