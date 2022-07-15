2bb5bb2df923e9f4059d9eddb8bab5a5269efcef
Bruno <104589217+brunott98@users.noreply.github.com>
2022-07-15 17:09:26 -0300

    Day and Money earned on screen

---
+++ for.js
@@ -0,0 +1,10 @@
+for(let j= 12; j<15;j++){
+    console.log(`i = ${j}`)
+}
+console.log("\n")
+
+const cash = [100,100,250,400,525]
+
+for(let i = 0; i<cash.length;i++){
+    console.log(`Day:${i+1}\nMoney earned:${cash[i]} \n`)
+}
