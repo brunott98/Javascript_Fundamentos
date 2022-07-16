7e02f1606e458444ef23986a6fc73c5f1b3976b0
Bruno <104589217+brunott98@users.noreply.github.com>
2022-07-16 18:14:32 -0300

    Add files via upload

--- for_in.js
+++ for_in.js
@@ -1,7 +1,23 @@
 const notas = [8,7,5,7]
-/*For i in, percorre os indices */
-for(i in notas){
+/*For i in, percorre os indices 
+Não esquece de usar o let para não acessar essa váriavel fora do escopo dela!
+*/
+for(let i in notas){
 
     console.log(`Indice:${i}\nNota:${notas[i]}\n`);
+
+}
+
+const character = {
+    Name:"Childe",
+    Codename: "Tartaglia",
+    Vision: "Hydro",
+    Constelation: "Monoceros Caeli"
+}
+
+
+for(let atributes in character){
+
+    console.log(`${atributes}: ${character[atributes]}`)
 
 }
