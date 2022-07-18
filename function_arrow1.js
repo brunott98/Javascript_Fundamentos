d79318c68df73fea264bd43a505e20143299379d
Bruno <104589217+brunott98@users.noreply.github.com>
2022-07-18 19:28:25 -0300

    Definindo Arrow functions

---
+++ function_arrow1.js
@@ -0,0 +1,15 @@
+let dobro =  function(a){
+
+    return 2* a;
+
+}
+
+dobrando1 = (a) => {  //Com bloco o return deve ser colocado
+    return 2 * a
+}
+
+triplicando = (a) => 3*a // return implicito  return 3*a, só vale para uma sentença
+
+
+console.log(dobrando1(2));
+console.log(triplicando(3));
