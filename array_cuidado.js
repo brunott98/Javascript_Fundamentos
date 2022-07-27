630cef7f1773c56d43ad4d5dcb9acddf5f10bacc
Bruno <104589217+brunott98@users.noreply.github.com>
2022-07-27 20:19:49 -0300

    Add files via upload

--- array_cuidado.js
+++ array_cuidado.js
@@ -6,4 +6,21 @@ myArray[11] = 999
 
 myArray.sort()
 console.log(myArray.length)  //12
-console.log(myArray)  //[ 1, 2, 3, 999, <8 empty items> ]
+
+delete myArray[0]  //Não reeordena, por isso é interessante usar o sort
+
+console.log(myArray)  //[ <1 empty item>, 2, 3, 999, <8 empty items> ]
+
+myDays = [12,15,19]
+
+myDays.splice(0,1,10)   //Posição, quantidade de elementos escolhidos e valor
+console.log(myDays)  //[ 10, 15, 19 ] 
+
+myDays.splice(0,1)   //Posição, quantidade de elementos escolhidos e valor
+console.log(myDays)  //[15, 19 ] 
+
+myDays.splice(0,1,100,155)   //Posição, quantidade de elementos escolhidos e valor
+console.log(myDays)  //[ 100, 155, 19 ]
+
+myDays.splice(0,3,222,444,888)   //Posição, quantidade de elementos escolhidos e valor
+console.log(myDays) //[ 222, 444, 888 ]
